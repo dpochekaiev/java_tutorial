9385897e67f16d029d402d50c11f4d5e01f07930
Dmitri Pochekaiev <dmitri.pochekaiev@cengage.com>
2019-03-13 18:18:49 +0200

    Created branch for code experiments

--- sandbox/src/main/java/com/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/com/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,24 @@
 package com.stqa.pft.sandbox;
 
+import static java.lang.System.*;
+
 public class MyFirstProgram {
 
 	public static void main (String[] args) {
-		System.out.println("Hello, world!");
+		out.println("Hello, world!");
+		int i = 2 + 2;
+		out.println("2 + 2 = " + i);
+		out.println("3 + 3 = " + (3+3));
+		out.println();
+
+		double x = 1 / 2.0;
+		out.println("1/2 = " + x); out.println();
+
+		int j = 2 + 2 * 3;
+		out.println("2 + 2 * 3 = " + j);
+
+		out.print("T"); out.print("his "); out.print("string");
+		out.print(" is "); out.println("concatenated.");
 	}
 
 }
