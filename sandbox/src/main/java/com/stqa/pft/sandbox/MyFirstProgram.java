3b618785cdd8b8cf35e608faacf53f5d88831ee7
Dmitri Pochekaiev <dmitri.pochekaiev@cengage.com>
2019-03-14 20:28:25 +0200

    Added Point class

--- sandbox/src/main/java/com/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/com/stqa/pft/sandbox/MyFirstProgram.java
@@ -26,12 +26,15 @@ public class MyFirstProgram {
 		out.println();
 
 		Square someKvadrat = new Square(5);
-
 		Rectangle somePriamougolnik = new Rectangle(4, 3);
+		Point p1  = new Point(0,0);
+		Point p2 = new Point(3,4);
 
 		out.println("Area of a square with a " + someKvadrat.sideLength + " side equals to: " + someKvadrat.area());
 		out.println("Area of a rectangle with sides " + somePriamougolnik.sideALength + " and "
 				+ somePriamougolnik.sideBLength + " equals to: " + somePriamougolnik.area());
+
+		out.println("The distance between p1 and p2 equals to: " + distance(p1, p2));
 	}
 
 	public static void hello () {
@@ -41,5 +44,10 @@ public class MyFirstProgram {
     public static void helloUserName (String userName) {
         out.println("Hello, " + userName + "!");
     }
+
+    public static double distance (Point p1, Point p2) {
+		return Math.sqrt(Math.pow((p1.xCoordinate - p2.xCoordinate), 2) +
+				Math.pow((p1.yCoordinate - p2.yCoordinate), 2));
+	}
 
 }
---
+++ sandbox/src/main/java/com/stqa/pft/sandbox/Point.java
@@ -0,0 +1,13 @@
+package com.stqa.pft.sandbox;
+
+public class Point {
+
+    public double xCoordinate;
+    public double yCoordinate;
+
+    public Point (double xCoordinate, double yCoordinate) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+}
