2d5c3bf23bdaf08827b19bd8519749ec05a765f2
Dmitri Pochekaiev <dmitri.pochekaiev@cengage.com>
2019-03-15 10:45:01 +0200

    Complited Home work #1

--- sandbox/src/main/java/com/stqa/pft/sandbox/PointDistanceEvaluator.java
+++ sandbox/src/main/java/com/stqa/pft/sandbox/PointDistanceEvaluator.java
@@ -5,16 +5,15 @@ import static java.lang.System.*;
 public class PointDistanceEvaluator {
 
     public static void main (String[] args) {
-        Point p1  = new Point(0,0);
+        Point p1 = new Point(0,0);
         Point p2 = new Point(3,4);
+        Point p3 = new Point(1,1);
 
-        out.println("The distance between p1 and p2 equals to: " + distance(p1, p2));
+        out.println("The distance between p1 and p2 equals to: " + p1.distance(p2));
+        out.println("The distance between p2 and p1 equals to: " + p2.distance(p1));
+        out.println("The distance between p1 and p1 equals to: " + p1.distance(p1));
+        out.println("The distance between p3 and p1 equals to: " + p3.distance(p1));
 
-    }
-
-    public static double distance (Point p1, Point p2) {
-        return Math.sqrt(Math.pow((p1.xCoordinate - p2.xCoordinate), 2) +
-                Math.pow((p1.yCoordinate - p2.yCoordinate), 2));
     }
 
 }
